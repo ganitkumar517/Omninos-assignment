bade91a3c6b41c90ba4c415f66b7364d39a6a30f
ganit kumar <jeetmehra517@gmail.com>
2024-05-01 13:28:03 +0530

    add todo api integration

--- src/components/Auth/Dashboard/Dashboard.jsx
+++ src/components/Auth/Dashboard/Dashboard.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Navbar/Navbar'
+import { useAddTodoMutation } from '../../../store/Api'
+import toast from 'react-hot-toast';
 
 export default function Dashboard() {
+    const [addTodo, { isLoading }] = useAddTodoMutation();
+    const [todo, setTodo] = useState('');
+    const handleAddTodo = () => {
+        addTodo({ todo })
+            .then(() => {
+                toast.success("Todo Added")
+                setTodo('')
+            })
+            .catch(() => toast.error("Todo Failed"));
+    }
     return (
         <div>
             <div>
                 <Navbar />
                 <div className='flex justify-between mx-10'>
                     <div className='flex mt-10 gap-4'>
-                        <input type='text' className='border p-2 bg-slate-200 rounded-xl w-96' placeholder='Add Todo'></input>
-                        <button className='bg-red-500 p-2 px-4 rounded-lg'>+Add</button>
+                        <input type='text' className='border p-2 bg-slate-200 rounded-xl w-96' placeholder='Add Todo' value={todo} onChange={(e) => setTodo(e.target.value)}></input>
+                        <button className='bg-red-500 p-2 px-4 rounded-lg' onClick={handleAddTodo} disabled={isLoading}>{isLoading ? <>Loading...</> : <>+Add</>}</button>
                     </div>
                     <div className='flex mt-10 gap-4'>
                         <input type='text' className='border p-2 bg-slate-200 rounded-xl w-96' placeholder='Search'></input>
--- src/store/Api.js
+++ src/store/Api.js
@@ -2,7 +2,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const fireApi = createApi({
   reducerPath: "fireApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000/",
+    prepareHeaders: (headers) => {
+      if (localStorage.getItem("token")) {
+        headers.set("Authorization", `Bearer ${localStorage.getItem("token")}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (body) => ({
@@ -18,7 +26,15 @@ export const fireApi = createApi({
         body,
       }),
     }),
+    addTodo: builder.mutation({
+      query: (body) => ({
+        method: "post",
+        url: "/todo",
+        body,
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation, useRegisterMutation } = fireApi;
+export const { useLoginMutation, useRegisterMutation, useAddTodoMutation } =
+  fireApi;
