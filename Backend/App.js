67cb422b2a37a4ac64c70a5ef27ee52046083af0
ganit kumar <jeetmehra517@gmail.com>
2024-05-01 21:44:31 +0530

    chore: api port changed

--- Backend/App.js
+++ Backend/App.js
@@ -181,7 +181,7 @@ app.post("/decodeToken", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("server is running on port");
