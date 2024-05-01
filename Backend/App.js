809ac73cdfad104683ab5f8ab8388b8ff80d406b
ganit kumar <jeetmehra517@gmail.com>
2024-05-01 18:58:55 +0530

    feat: get, edit, delete todo api

--- Backend/App.js
+++ Backend/App.js
@@ -98,6 +98,69 @@ app.post("/todo", authenticateToken, async (req, res) => {
       .json({ message: "Failed to create todo", error: error.message });
   }
 });
+app.get("/todo", authenticateToken, async (req, res) => {
+  const { search } = req.query;
+  let query = {};
+
+  if (search) {
+    query = { todo: { $regex: search, $options: "i" } };
+  }
+
+  try {
+    const todos = await Todo.find(query);
+    res.status(200).json({ todos });
+  } catch (error) {
+    console.error("Error fetching todos:", error);
+    res
+      .status(500)
+      .json({ message: "Failed to fetch todos", error: error.message });
+  }
+});
+
+app.put("/todo/:id", authenticateToken, async (req, res) => {
+  const { id } = req.params;
+  const { updatedTodo } = req.body;
+
+  try {
+    const todoToUpdate = await Todo.findById(id);
+    if (!todoToUpdate) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    todoToUpdate.todo = updatedTodo;
+    await todoToUpdate.save();
+
+    res
+      .status(200)
+      .json({ message: "Todo updated successfully", todo: todoToUpdate });
+  } catch (error) {
+    console.error("Error updating todo:", error);
+    res
+      .status(500)
+      .json({ message: "Failed to update todo", error: error.message });
+  }
+});
+app.delete("/todo/:id", authenticateToken, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const todoToDelete = await Todo.findById(id);
+    if (!todoToDelete) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    await Todo.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "Todo deleted successfully",
+      deletedTodo: todoToDelete,
+    });
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+    res
+      .status(500)
+      .json({ message: "Failed to delete todo", error: error.message });
+  }
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
