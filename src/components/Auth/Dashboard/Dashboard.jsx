c9cc0bb5c039f0aec1b0e1e84b8d756c2220fced
ganit kumar <jeetmehra517@gmail.com>
2024-05-01 16:43:16 +0530

    feat: search and add input in dashboard

--- src/components/Auth/Dashboard/Dashboard.jsx
+++ src/components/Auth/Dashboard/Dashboard.jsx
@@ -4,15 +4,21 @@ import Navbar from '../Navbar/Navbar'
 export default function Dashboard() {
     return (
         <div>
-            <Navbar />
             <div>
-                <div className='flex justify-center mt-10 gap-4'>
-                    <input type='text' className='border p-2 bg-slate-200 rounded-xl w-96' placeholder='Search'></input>
-                    <button className='bg-blue-500 p-2 px-4 rounded-lg'>Search</button>
+                <Navbar />
+                <div className='flex justify-between mx-10'>
+                    <div className='flex mt-10 gap-4'>
+                        <input type='text' className='border p-2 bg-slate-200 rounded-xl w-96' placeholder='Add Todo'></input>
+                        <button className='bg-red-500 p-2 px-4 rounded-lg'>+Add</button>
+                    </div>
+                    <div className='flex mt-10 gap-4'>
+                        <input type='text' className='border p-2 bg-slate-200 rounded-xl w-96' placeholder='Search'></input>
+                        <button className='bg-blue-500 p-2 px-4 rounded-lg'>Search</button>
+                    </div>
                 </div>
-                <div>
+            </div>
+            <div>
 
-                </div>
             </div>
         </div>
     )
